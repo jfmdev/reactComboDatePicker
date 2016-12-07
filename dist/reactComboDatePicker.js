72ba361902b1ac7a13f2fcc0d2fbef41cc67326d
Jose <jfmcode@gmail.com>
2016-12-07 18:47:53 -0300

    Change license to MPL 2.0

--- dist/reactComboDatePicker.js
+++ dist/reactComboDatePicker.js
@@ -14,7 +14,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
  * reactComboDatePicker v1.0.0
  * http://github.com/jfmdev/reactComboDatePicker
  * «Copyright 2016 Jose F. Maldonado»
- * License: LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0.html)
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 var ComboDatePicker = function (_React$Component) {
--- source/reactComboDatePicker.js
+++ source/reactComboDatePicker.js
@@ -2,7 +2,7 @@
  * reactComboDatePicker v1.0.0
  * http://github.com/jfmdev/reactComboDatePicker
  * «Copyright 2016 Jose F. Maldonado»
- * License: LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0.html)
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 class ComboDatePicker extends React.Component {
